5ed8b2fd226cd9a9daebad52a8001d3747a60892
OFrede <moio99@gmail.com>
2025-05-06 15:33:43 +0200

    proba fly

--- services/bibliotecas.js
+++ services/bibliotecas.js
@@ -24,7 +24,7 @@ async function getBibliotecas(idUsuario){
 async function getBiblioteca(idUsuario, id){
   console.log('Petiçom de getBiblioteca ' + new Date().toJSON());
   const dadosBiblioteca = await db.query(
-    `SELECT b.idBiblioteca as id, b.Nome as nome, b.Endereco as Endereco, b.Localidade as localidade
+    `SELECT b.idBiblioteca as id, b.Nome as nome, b.Endereco as endereco, b.Localidade as localidade
       , b.Telefone as telefone, b.DataAsociamento as dataAsociamento, b.DataRenovacom as dataRenovacom
       , b.Comentario as comentario
       FROM Biblioteca b
