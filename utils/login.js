7b6266df7503a10658e3845bdc3f1bb93d559e44
OFrede <moio99@gmail.com>
2026-08-18 13:23:36 +0200

    outro renomeado

--- utils/login.js
+++ utils/login.js
@@ -14,7 +14,7 @@ loginRouter.post('/', async (request, response) => {
   }
 
   const rows = await db.query(
-    `SELECT * FROM usuario WHERE nome = '${nome}' AND pass = '${contrasinal}' AND ativado = true;`
+    `SELECT Nome AS nome, idUsuario AS idusuario, fkIdioma AS fkidioma FROM usuario WHERE nome = '${nome}' AND pass = '${contrasinal}' AND ativado = true;`
   );
   if (rows.length === 0) {
     return response.status(401).json({
@@ -22,9 +22,9 @@ loginRouter.post('/', async (request, response) => {
     })
   } else {
     const userForToken = {
-      nome: rows[0].Nome,
-      id: rows[0].idUsuario,
-      idioma: rows[0].fkIdioma
+      nome: rows[0].nome,
+      id: rows[0].idusuario,
+      idioma: rows[0].fkidioma
     }
     // o token expira em uma hora (60*60 segundos)
     const token = jwt.sign(userForToken, process.env.SEGREDO_PARA_O_TOKEN, { expiresIn: 60*60 });
